51e133875ea0469b96227d2836a5afd2beb1abe4
Console, Inc <iConsole@users.noreply.github.com>
2016-01-03 14:11:30 -0800

    Update minnowboard_max.mk
    Model should just be MAX. Make is technically Minnowboard as it is techncially a Minnow foundation product.

--- minnowboard_max/minnowboard_max.mk
+++ minnowboard_max/minnowboard_max.mk
@@ -21,7 +21,7 @@ $(call inherit-product,device/generic/common/x86.mk)
 PRODUCT_NAME := minnowboard_max
 PRODUCT_BRAND := Minnowboard
 PRODUCT_DEVICE := minnowboard_max
-PRODUCT_MODEL := Minnowboard MAX
+PRODUCT_MODEL := MAX
 PRODUCT_LOCALES := en_US en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ pl_PL ja_JP zh_TW zh_CN zh_HK ru_RU ko_KR nb_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG ca_ES en_GB fi_FI hi_IN hr_HR hu_HU in_ID iw_IL lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA vi_VN tl_PH ar_EG fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN en_XA ar_XB fr_CA km_KH lo_LA ne_NP mn_MN hy_AM az_AZ ka_GE de_AT de_CH de_LI en_AU en_CA en_NZ en_SG eo_EU fr_CH fr_BE it_CH nl_BE
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
