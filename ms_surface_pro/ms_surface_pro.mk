ec9fb6ce821c23dd8fbd759a6561f8d5ffb8ffef
Console <howdy@iconsole.tv>
2016-02-04 21:54:34 -0800

    Adding Surface Pro (woot). Now, on to Pro 2!

---
+++ ms_surface_pro/ms_surface_pro.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2016 Console, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# includes the base of Android-x86 platform
+$(call inherit-product,device/generic/common/x86.mk)
+
+# Overrides
+PRODUCT_NAME := ms_surface_pro
+PRODUCT_BRAND := Microsoft
+PRODUCT_DEVICE := ms_surface_pro
+PRODUCT_MODEL := Microsoft Surface Pro
+PRODUCT_LOCALES := en_US en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ pl_PL ja_JP zh_TW zh_CN zh_HK ru_RU ko_KR nb_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG ca_ES en_GB fi_FI hi_IN hr_HR hu_HU in_ID iw_IL lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA vi_VN tl_PH ar_EG fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN en_XA ar_XB fr_CA km_KH lo_LA ne_NP mn_MN hy_AM az_AZ ka_GE de_AT de_CH de_LI en_AU en_CA en_NZ en_SG eo_EU fr_CH fr_BE it_CH nl_BE
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_AAPT_CONFIG := normal large mdpi tvdpi
+PRODUCT_AAPT_PREF_CONFIG := tvdpi
